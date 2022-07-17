f91d2f83a093149191b6321b961b8d60843e3736
mgarciate <mgarciate@gmail.com>
2022-07-17 13:29:06 +0200

    Fix #4 Ability to add keys from wizard when necessary

--- neard_image/entrypoint.sh
+++ neard_image/entrypoint.sh
@@ -7,25 +7,36 @@ NEARD_FLAGS=${NEAR_HOME:+--home="$NEAR_HOME"}
 mkdir -p $NEAR_HOME/data
 
 if [ ! -f ${NEAR_HOME}/node_key.json ]; then
+    echo "Initializing node, this can take a while..."
     cmd="neard $NEARD_FLAGS init ${CHAIN_ID:+--chain-id=$CHAIN_ID} ${ACCOUNT_ID:+--account-id=$ACCOUNT_ID} --download-genesis --download-config"
     $cmd
 fi
-echo "Downloading a snapshot, this can take a while..."
 if [ ! -f ${NEAR_HOME}/data/CURRENT ]; then
+    echo "Downloading a snapshot, this can take a while..."
     axel $SNAPSHOT_URL -o ${NEAR_HOME}
     tar -xvf ${NEAR_HOME}/data.tar -C ${NEAR_HOME}/data
     rm ${NEAR_HOME}/data.tar
 fi
 
-if [ -n "$NODE_KEY" ]
+if [ -n "$ACCOUNT_ID" ] && [ -n "$NODE_PUBLIC_KEY" ] && [ -n "$NODE_SECRET_KEY" ]
 then
+    echo "Configure custom node_key.json"
     cat << EOF > "$NEAR_HOME/node_key.json"
-{"account_id": "", "public_key": "", "secret_key": "$NODE_KEY"}
+{"account_id": "$ACCOUNT_ID", "public_key": "$NODE_PUBLIC_KEY", "secret_key": "$NODE_SECRET_KEY"}
+EOF
+fi
+
+if [ -n "$ACCOUNT_ID" ] && [ -n "$VALIDATOR_PUBLIC_KEY" ] && [ -n "$VALIDATOR_SECRET_KEY" ]
+then
+    echo "Configure custom validator_key.json"
+    cat << EOF > "$NEAR_HOME/validator_key.json"
+{"account_id": "$ACCOUNT_ID", "public_key": "$VALIDATOR_PUBLIC_KEY", "secret_key": "$VALIDATOR_SECRET_KEY"}
 EOF
 fi
 
 ulimit -c unlimited
 
+# Both environment variables are not necessary since both values are into config.json
 echo "Telemetry: ${TELEMETRY_URL}"
 echo "Bootnodes: ${BOOT_NODES}"
 
