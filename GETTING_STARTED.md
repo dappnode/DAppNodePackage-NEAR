f724c553770e021fd7f1e297556366799be68a5f
Luka Gulin <lg@shardlabs.io>
2025-02-03 13:08:54 +0100

    fixing snapshot, enabling validator key

--- GETTING_STARTED.md
+++ GETTING_STARTED.md
@@ -1,11 +1,8 @@
-This package contains a NEAR Network Node and a Local NEAR Wallet.
-
-**Wallet URL**: [wallet.near.dappnode](http://wallet.near.dappnode)
+This package contains a NEAR Network Node.
 
 **Node URL**: [near.dappnode](http://near.dappnode:3030)
 
 ⚠️**Important**⚠️
-You will have to wait for your node to fully synchronize before you can connect the wallet to your node.
 
 It includes a Grafana dashboard for [DMS](http://dms.dappnode/d/nearuid/near-overview?orgId=1&refresh=10s&from=now-3h&to=now)
 
--- neard_image/start_near.sh
+++ neard_image/start_near.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 export NEAR_HOME=/srv/near
-CHAIN_ID=mainnet
-SNAPSHOT_URL="s3://near-protocol-public/backups/$CHAIN_ID/rpc"
-FULL_ACCOUNT_ID="$ACCOUNT_ID.$CONTRACT_NAME"
+
+FULL_ACCOUNT_ID="$ACCOUNT_ID.$STAKING_CONTRACT_NAME"
 NEARD_FLAGS=${NEAR_HOME:+--home="$NEAR_HOME"}
 mkdir -p $NEAR_HOME/data
 
@@ -12,20 +11,18 @@ if [ ! -f ${NEAR_HOME}/node_key.json ]; then
     cmd="neard $NEARD_FLAGS init --chain-id=${CHAIN_ID} ${FULL_ACCOUNT_ID:+--account-id=$FULL_ACCOUNT_ID} --download-genesis --download-config"
     $cmd
 fi
-if [ ! -f ${NEAR_HOME}/data/CURRENT ]; then
-    echo "Downloading a snapshot, this can take a while..."
-    aws s3 --no-sign-request cp $SNAPSHOT_URL/latest .
-    LATEST=$(cat latest)
-    aws s3 --no-sign-request cp --no-sign-request --recursive $SNAPSHOT_URL/$LATEST ${NEAR_HOME}/data
+if [ "$DOWNLOAD_SNAPSHOT" = "true" ] && [ ! -f "${NEAR_HOME}/data/CURRENT" ]; then
+    curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/fastnear/static/refs/heads/main/down_rclone.sh \
+         | THREADS=16 DATA_PATH="$NEAR_HOME/data" CHAIN_ID="$CHAIN_ID" RPC_TYPE="fast-rpc" bash
 fi
 
-# if [ -n "$ACCOUNT_ID" ] && [ -n "$VALIDATOR_PUBLIC_KEY" ] && [ -n "$VALIDATOR_SECRET_KEY" ]
-# then
-#     echo "Configure custom validator_key.json"
-#     cat << EOF > "$NEAR_HOME/validator_key.json"
-# {"account_id": "$FULL_ACCOUNT_ID", "public_key": "$VALIDATOR_PUBLIC_KEY", "secret_key": "$VALIDATOR_SECRET_KEY"}
-# EOF
-# fi
+if [ -n "$ACCOUNT_ID" ] && [ -n "$VALIDATOR_PUBLIC_KEY" ] && [ -n "$VALIDATOR_SECRET_KEY" ]
+then
+    echo "Configure custom validator_key.json"
+    cat << EOF > "$NEAR_HOME/validator_key.json"
+{"account_id": "$FULL_ACCOUNT_ID", "public_key": "$VALIDATOR_PUBLIC_KEY", "secret_key": "$VALIDATOR_SECRET_KEY"}
+EOF
+fi
 
 ulimit -c unlimited
 
@@ -33,4 +30,18 @@ ulimit -c unlimited
 echo "Telemetry: ${TELEMETRY_URL}"
 echo "Bootnodes: ${BOOT_NODES}"
 
-exec neard "$NEARD_FLAGS" run ${TELEMETRY_URL:+--telemetry-url="$TELEMETRY_URL"} ${BOOT_NODES:+--boot-nodes="$BOOT_NODES"} "$@"
+
+LATEST_PEERS=$(curl -X POST https://rpc.${CHAIN_ID}.near.org \  -H "Content-Type: application/json" \
+  -d '{
+        "jsonrpc": "2.0",
+        "method": "network_info",
+        "params": [],
+        "id": "dontcare"
+      }' | \
+jq '.result.active_peers as $list1 | .result.known_producers as $list2 |
+$list1[] as $active_peer | $list2[] |
+select(.peer_id == $active_peer.id) |
+"\(.peer_id)@\($active_peer.addr)"' |\
+awk 'NR>2 {print ","} length($0) {print p} {p=$0}' ORS="" | sed 's/"//g')
+
+exec neard "$NEARD_FLAGS" run ${TELEMETRY_URL:+--telemetry-url="$TELEMETRY_URL"}  --boot-nodes $LATEST_PEERS "$@"
